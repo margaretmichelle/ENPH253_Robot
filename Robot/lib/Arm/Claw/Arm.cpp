fc0a238c2c3522371b3466aaf3e12eb5553108d9
Cole Rowell <cxlerxwell@gmail.com>
2022-07-17 21:57:35 -0700

    Started firmware for Claw and Arm (doesn'tcompile)

--- Robot/lib/Arm/Claw/Arm.cpp
+++ Robot/lib/Arm/Claw/Arm.cpp
@@ -1,5 +1,26 @@
-#include <Servo.h>
-#include <Arduino.h>
-#include <Constants.h>
-#include <Claw.h>
+#include <Arm.h>
 
+using namespace ArmConstants;
+
+Arm::Arm(int clawControlPin, int armControlPin){
+    this.controlPin = armControlPin;
+    ArmServo.attach(armControlPin);
+    ArmServo.write(CLAW_UP);
+    currentPos =  CLAW_UP;
+    ArmClaw(armControlPin);
+}
+
+void Arm::returnToHome(){
+    ArmServo.write(CLAW_UP);
+}
+
+int Arm::getPosition(){
+    return currentPos;
+}
+
+void Arm::placeObjectInContainer(){
+    ArmServo.write(CLAW_DOWN)
+    //Add Hall Effect Sensing
+    ArmClaw.close();
+    returnToHome();
+}
--- Robot/lib/Arm/Claw/Claw.cpp
+++ Robot/lib/Arm/Claw/Claw.cpp
@@ -1,3 +1,24 @@
-#include <Arduino.h>
-#include <Constants.h>
-#include <Servo.h>
+#include <Claw.h>
+
+using namespace ClawConstants;
+
+Claw::Claw(int controlPin){
+    this.controlPin = controlPin;
+    clawServo.attach(controlPin);
+    currentPos = CLAW_OPEN;
+    clawServo.write(CLAW_OPEN);
+}
+
+void Claw::close(){
+    currentPos = CLAW_OPEN;
+    clawServo.write(CLAW_OPEN);
+}
+
+void Claw::open(){
+    currentPos = CLAW_CLOSED;
+    clawServo.write(CLAW_CLOSED);
+}
+
+int Claw::getPosition(){
+    return currentPos;
+}
