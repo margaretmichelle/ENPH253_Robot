0ab71a48e0116c0a1e0332aa9621559721b91a7f
laurylee <laurynl@student.ubc.ca>
2022-07-20 15:58:23 -0700

    Added test IR code

---
+++ lib/IR/IR.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <Constants.h>
+#include <Motor.h>
+#include <IR.h>
+#include <math.h>
+
+IR::IR(Motor leftMotor, Motor rightMotor, int motorSpeed):leftMotor(leftMotor), rightMotor(rightMotor), motorSpeed(motorSpeed){
+  leftSensor = IRFollowerNS::LEFT_IR_SENSOR;
+  middleSensor = IRFollowerNS::MIDDLE_IR_SENSOR;
+  rightSensor = IRFollowerNS::RIGHT_IR_SENSOR;
+
+  pinMode(leftSensor, INPUT_PULLUP);
+  pinMode(middleSensor, INPUT_PULLUP);
+  pinMode(rightSensor,INPUT_PULLUP);
+}
+
+void IR::followIR() {
+    leftIRReading = readIRSensor(leftSensor);
+    rightIRReading = readIRSensor(rightSensor);
+    middleIRReading = readIRSensor(middleSensor);
+
+    if(fabs(leftIRReading - rightIRReading) >= IRFollowerNS::IR_DIFF_THRESHOLD) {
+        if(leftIRReading > rightIRReading) {
+            leftMotorSpeed = motorSpeed + IRFollowerNS::MOTOR_SPEED_INCREMENT;
+        } else {
+            rightMotorSpeed = motorSpeed + IRFollowerNS::MOTOR_SPEED_INCREMENT;
+        }
+    } else {
+        if(middleIRReading > leftIRReading && middleIRReading > rightIRReading) {
+            leftMotorSpeed = motorSpeed;
+            rightMotorSpeed = motorSpeed;
+        }
+    }
+
+    leftMotor.speed(leftMotorSpeed);
+    rightMotor.speed(rightMotorSpeed);
+};
+
+double IR::readIRSensor(int pinNumber){
+    return analogRead(pinNumber);
+};
