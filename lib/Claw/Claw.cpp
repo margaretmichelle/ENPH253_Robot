0e7a8fd2602e7411d93efed33bd4bb98525b7e4a
margaretmichelle <margaretmichelle6@gmail.com>
2022-07-24 20:47:47 -0700

    Fixed claw and servo code to move servos

--- lib/Claw/Claw.cpp
+++ lib/Claw/Claw.cpp
@@ -12,21 +12,20 @@ Claw::Claw(int controlPin, int hallEffectPin) : clawServo(controlPin) {
 
 void Claw::close() {
     // Assume CLAW_OPEN > CLAW_CLOSED
-    for (int i = ClawNS::CLAW_OPEN; i >= ClawNS::CLAW_CLOSED; i--) {
-        if (analogRead(hallEffectPin) < 300) {
-            magnetFound = true;
-            clawServo.write(ClawNS::CLAW_OPEN);
-            break;
-        }
-        clawServo.write(i);
-    }
+    // for (int i = ClawNS::CLAW_OPEN; i >= ClawNS::CLAW_CLOSED; i - 10) {
+    //     if (analogRead(hallEffectPin) < 300) {
+    //         magnetFound = true;
+    //         // clawServo.write(ClawNS::CLAW_OPEN);
+    //         // break;
+    //     }
+    //     clawServo.write(i);
+    // }
+    clawServo.write(ClawNS::CLAW_CLOSED);
 }
 
 void Claw::open() {
     clawServo.write(ClawNS::CLAW_OPEN);
 }
-
-
 
 int Claw::getPosition() {
     return clawServo.getPosition();
--- lib/Servo/ServoMotor.cpp
+++ lib/Servo/ServoMotor.cpp
@@ -8,8 +8,10 @@ ServoMotor::ServoMotor(int controlPin) {
 }
  
 void ServoMotor::write(int angle) {
+  int delayTime = abs(angle - currentPos) * 1000 / 180;
   pwm_start(pinName, ServoNS::SERVO_FREQ, mapAngleToDutyCycle(angle), RESOLUTION_12B_COMPARE_FORMAT);
   currentPos = angle;
+  delay(delayTime);
 }
 
 int ServoMotor::mapAngleToDutyCycle(int angle) {
