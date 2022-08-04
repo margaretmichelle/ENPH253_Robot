0a2092c808f52834dc86fc1f128c114a5c929241
Cole Rowell <cxlerxwell@gmail.com>
2022-08-04 16:51:46 -0700

    Added comments

--- lib/Encoder/Encoder.cpp
+++ lib/Encoder/Encoder.cpp
@@ -28,40 +28,50 @@ void Encoder::driveStraight(int distance, int motorPower){
     int leftPower = motorPower;
     int rightPower = motorPower;
 
+    // Sets target number of counts for the encoder
     float numOfRevs = distance / EncoderNS::ROTATION_DISTANCE_MM;
     unsigned long targetCount = numOfRevs * EncoderNS::PULSES_PER_ROTATION;
 
+    // Sets inital encoder counts to keep track of new changes to encoder counts
     unsigned long initalLeftCount = leftEncoderPulses;
     unsigned long initalRightCount = rightEncoderPulses;
 
-    unsigned long prevLeftCount = leftEncoderPulses;
-    unsigned long prevRightCount = rightEncoderPulses;
-
+    //travels until one wheel reaches the end
     while ( (leftEncoderPulses - initalLeftCount) < targetCount && rightEncoderPulses - initalRightCount < targetCount ){
+        // Chooses a sample count, have to do it this way since the interupted value will constantly change
         unsigned long currentLeftCount = leftEncoderPulses;
         unsigned long currentRightCount = rightEncoderPulses;
 
+        //Looks at the change in encoder pulses from the initial code to see what motor needs to travel more
         unsigned long diffLeft = currentLeftCount - initalLeftCount;
         unsigned long diffRight = currentRightCount - initalRightCount;
 
-        //unsigned long diffLeft = currentLeftCount - prevLeftCount;
-        //unsigned long diffRight = currentRightCount - prevRightCount;
+        //Some code I found online uses this I wanted to test out mine first, IDK why they do it this way
+        /*
+        unsigned long diffLeft = currentLeftCount - prevLeftCount;
+        unsigned long diffRight = currentRightCount - prevRightCount;
 
-        //unsigned long prevLeftCount = currentLeftCount;
-        //unsigned long prevRightCount = currentRightCount;
+        unsigned long prevLeftCount = currentLeftCount;
+        unsigned long prevRightCount = currentRightCount;
+        */
 
+        //Increase left's speed if right has travelled more
         if (diffLeft < diffRight){
             rightPower -= EncoderNS::POWER_OFFSET;
             leftPower += EncoderNS::POWER_OFFSET;
         }
+
+        //Increase right's speed if left has travelled more
         else if(diffLeft > diffRight){
             rightPower += EncoderNS::POWER_OFFSET;
             leftPower -= EncoderNS::POWER_OFFSET;
         }
 
+        //delay to give motors time to change speeds
         delay(20);
     }
 
+    //stop moving motors
     LeftMotor.stop();
     RightMotor.stop();
 }
